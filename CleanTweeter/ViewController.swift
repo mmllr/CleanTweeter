06045bc71071b5dff6c58f3180a72646f8a3d290
Markus Mueller <mmlr@gmx.net>
2014-10-29 16:06:23 +0100

    Initial Commit

---
+++ CleanTweeter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CleanTweeter
+//
+//  Created by Markus Müller on 29.10.14.
+//  Copyright (c) 2014 Markus Müller. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
